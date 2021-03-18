4fb10b36f050654414f490ab710454ac0b12de8c
APresekal <presekal@ui.ac.id>
2021-03-18 17:41:54 +0100

    Add files via upload

--- r08.py
+++ r08.py
@@ -15,28 +15,28 @@ cursor = conn.cursor()
 #Value id
 cursor.execute('''SELECT value from objects WHERE id=67''')
 result = cursor.fetchone()
-global record1 = result[0]
+record1 = result[0]
 cursor.execute('''SELECT value from objects WHERE id=68''')
 result = cursor.fetchone()
-global record2 = result[0]
+record2 = result[0]
 cursor.execute('''SELECT value from objects WHERE id=69''')
 result = cursor.fetchone()
-global record3 = result[0]
+record3 = result[0]
 cursor.execute('''SELECT value from objects WHERE id=70''')
 result = cursor.fetchone()
-global record4 = result[0]
+record4 = result[0]
 cursor.execute('''SELECT value from objects WHERE id=71''')
 result = cursor.fetchone()
-global record5 = result[0]
+record5 = result[0]
 cursor.execute('''SELECT value from objects WHERE id=72''')
 result = cursor.fetchone()
-global record6 = result[0]
+record6 = result[0]
 cursor.execute('''SELECT value from objects WHERE id=73''')
 result = cursor.fetchone()
-global record7 = result[0]
+record7 = result[0]
 cursor.execute('''SELECT value from objects WHERE id=74''')
 result = cursor.fetchone()
-global record8 = result[0]
+record8 = result[0]
 
 #Value code
 cursor.execute('''SELECT code from objects WHERE id=67''')
@@ -64,34 +64,29 @@ cursor.execute('''SELECT code from objects WHERE id=74''')
 result = cursor.fetchone()
 r8 = result[0]
 
-print(record1)
-print(record8)
-print(r1)
-print(r8)
 
 # Define a function for the thread
-def server08():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s1:
-        s1.bind(('',PORT1))
-        s1.listen()
-        conn1, addr = s1.accept()
-        with conn1:
-            print('Server 1 from:',addr)
-            while True:
-                a = 1
-                while a < 6:
-                    #Format: mu01_id+value
-                    cursor.execute('''SELECT value from objects WHERE id=67''')
-                    result = cursor.fetchone()
-                    print(result)
-                    print(record1)
-                    if record1 != result[0]:
-                        print(result[0])
-                        string = "mu01_"+str(r1)+"+"+str(result[0])
-                        datax = string.encode()
-                        s1.sendall(datax)
-                        print(string)
-                        record1 = result[0]
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s1:
+    s1.bind(('',PORT1))
+    s1.listen()
+    conn1, addr = s1.accept()
+    with conn1:
+        print('Server 1 from:',addr)
+        while True:
+            a = 1
+            while a < 6:
+                #Format: mu01_id+value
+                cursor.execute('''SELECT value from objects WHERE id=67''')
+                result = cursor.fetchone()
+                if record1 != result[0]:
+                    print(result[0])
+                    string = "mu01_"+str(r1)+"+"+str(result[0])
+                    datax = string.encode()
+                    s1.sendall(datax)
+                    print(string)
+                    record1 = result[0]
+
+                    """
 
                     cursor.execute('''SELECT value from objects WHERE id=68''')
                     result = cursor.fetchone()
@@ -162,9 +157,10 @@ def server08():
                         s1.sendall(datax)
                         print(string)
                         record8 = result[0]
+                """
 
 
-
+"""
 try:                   
     _thread.start_new_thread( server08, ( ) )
 
@@ -173,4 +169,4 @@ except:
 
 while 1:
    pass
-
+"""
